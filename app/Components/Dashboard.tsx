0bea063de89cb2daf40af76934dbf6c46f42536e
Joenel <joenelsevellejo831@gmail.com>
2024-09-25 23:14:46 +0800

    added dashboard

---
+++ app/Components/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='w-full h-screen bg-gray-950 flex justify-center items-center'>
+        <h1>Dashboard</h1>
+    </div>
+  )
+}
