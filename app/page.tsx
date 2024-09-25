53b93c520c0fc34eaef4e9343de62d9e83fb186f
Mark Justin Sayson <jusssayson@gmail.com>
2024-09-25 22:33:03 +0800

    SCRUM-17: TESTING LANGS

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Sayson first comment
+            Sayson Front-End Developer w/ 3 months exeperience in Data Science
           </a>
         </div>
       </main>
