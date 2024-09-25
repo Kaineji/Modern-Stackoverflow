fdbc5544f09927b4956a14852a758736aca6b496
Lucky Estrada <157455992+STI-TakoYucky@users.noreply.github.com>
2024-09-25 23:28:01 +0800

    second commit (#9)

--- app/Components/Dashboard.tsx
+++ app/Components/Dashboard.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Dashboard() {
   return (
-    <div className='w-full h-screen bg-gray-950 flex justify-center items-center'>
+    <div className='w-full h-screen bg-gray-950 flex justify-center items-center flex-col'>
         <h1>Dashboard</h1>
+        <button className='p-3 bg-white text-black rounded-lg'>Open</button>
     </div>
   )
 }
