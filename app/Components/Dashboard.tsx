f1a125efb7b0e1f0deed3bbc219b539a65b18056
Joenel Seve <joenelsevellejo831@gmail.com>
2024-09-27 07:28:27 +0800

    Joenel branch (#12)
    * i change it to plase
    
    * SCRUM-17: Sayson first commit
    
    * SCRUM-1: Lucky first commit
    
    * Scrum 17 (#7)
    
    * SCRUM-17: TESTING LANGS
    
    * SCRUM-17: TRY LANG
    
    * Tobi (#8)
    
    * added changes in team project
    
    * added dashboard
    
    ---------
    
    Co-authored-by: Joenel <joenelsevellejo831@gmail.com>
    
    * second commit (#9)
    
    * Joenel Folder
    
    ---------
    
    Co-authored-by: Kaineji <115143006+Kaineji@users.noreply.github.com>
    Co-authored-by: Mark Justin Sayson <jusssayson@gmail.com>
    Co-authored-by: Lucky Estrada <estrada.lnp@gmail.com>
    Co-authored-by: Lucky Estrada <157455992+STI-TakoYucky@users.noreply.github.com>

---
+++ app/Components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className='w-full h-screen bg-gray-950 flex justify-center items-center flex-col'>
+        <h1>Dashboard</h1>
+        <button className='p-3 bg-white text-black rounded-lg'>Open</button>
+    </div>
+  )
+}
