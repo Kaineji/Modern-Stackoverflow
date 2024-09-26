cd4b25b884652e2c78d5718cd1b6a09f1c5dc182
Joenel <joenelsevellejo831@gmail.com>
2024-09-26 21:43:22 +0800

    Joenel Folder

---
+++ app/Joenel_Folder/Joenel_Components/DashBoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DashBoard() {
+  return (
+    <div className='h-screen bg-gray-950 flex justify-center items-center'>
+      <h1>Joenel Dashboard</h1>
+    </div>
+  )
+}
